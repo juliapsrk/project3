0beb4982ca6971a6e15d1e0478fd9af93004cd58
Julia <juliapsrk@gmail.com>
2022-07-07 21:58:10 +0200

    julia what the fuck

--- server/models/user.js
+++ server/models/user.js
@@ -3,41 +3,44 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
 
-const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please enter a name'],
-    minlength: [4, 'Minimum Name length is 4 characters'],
-    trim: true
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter a name'],
+      minlength: [4, 'Minimum Name length is 4 characters'],
+      trim: true
+    },
+    email: {
+      type: String,
+      required: [true, 'Please enter an email'],
+      unique: [true, ' That email is already registered'],
+      lowercase: true,
+      validate: [isEmail, 'Please enter a valid email'],
+      trim: true
+    },
+    passwordHashAndSalt: {
+      type: String,
+      required: [true, 'Please enter an email'],
+      minlength: [6, 'Minimum Password length is 6 characters']
+    },
+    description: {
+      type: String,
+      maxLength: 5000,
+      trim: true
+    },
+    userType: {
+      type: String,
+      required: true,
+      default: 'private',
+      enum: ['private', 'center']
+    },
+    picture: {
+      type: String
+    }
   },
-  email: {
-    type: String,
-    required: [true, 'Please enter an email'],
-    unique: [true, ' That email is already registered'],
-    lowercase: true,
-    validate: [isEmail, 'Please enter a valid email'],
-    trim: true
-  },
-  passwordHashAndSalt: {
-    type: String,
-    required: [true, 'Please enter an email'],
-    minlength: [6, 'Minimum Password length is 6 characters'],
-  },
-  description: {
-    type: String,
-    maxLength: 5000,
-    trim: true
-  },
-  userType: {
-    type: String,
-    required: true,
-    default: 'private',
-    enum: ['private', 'center']
-  },
-  picture: {
-    type: String
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', schema);
 
