9a34904e6835f3efe72c9dd28729d280e60dd4c4
Julia <juliapsrk@gmail.com>
2022-07-08 18:54:38 +0200

    Julia: Community Update 2

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
+import './PostCard.scss';
 
 const PostCard = ({ post }) => (
-  <Link className='post-card' to={`/post/${post._id}`}>
-    <span>{post.title}</span>
-  </Link>
+  <div className='post-card'>
+    <Link to={`/post/${post._id}`}>
+      <span>{post.title}</span>
+      <br />
+      <p>{post.description}</p>
+      <small>{new Date(post.createdAt).toGMTString()}</small>
+    </Link>
+  </div>
 );
 
 export default PostCard;
